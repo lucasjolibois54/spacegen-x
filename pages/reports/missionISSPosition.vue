7808bdc4c7736c6b0bcef6ba85ea3227a474f9a7
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-08 17:49:31 +0200

    Update missionISSPosition.vue

--- pages/reports/missionISSPosition.vue
+++ pages/reports/missionISSPosition.vue
@@ -1,13 +1,11 @@
 <script setup>
-
+import OlMap from '@/components/mission-map/OlMap.vue';
 </script>
-
 <template>
     <div>
-        <h1>ISS Position</h1>
+      <h1>My OpenLayers Map</h1>
+      <OlMap />
     </div>
-</template>
+  </template>
 
-<style scoped>
-
-</style>
+  
