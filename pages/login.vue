8cec2f2a12c1a57f731a8d33eb5fa7157d5b71ff
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-08 10:41:35 +0200

    Update login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -3,10 +3,33 @@
 </script>
 
 <template>
-    <div>
-        Login
+    <div class="min-h-screen flex items-center justify-center bg-gray-200 p-6">
+        <div class="bg-white p-8 rounded shadow-md w-96">
+            <h1 class="text-2xl mb-4">Login</h1>
+
+            <div class="mb-4">
+                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                <input 
+                    type="email" 
+                    id="email"
+                    placeholder="you@example.com" 
+                    class="mt-1 p-2 w-full border rounded-md">
+            </div>
+
+            <div class="mb-6">
+                <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                <input 
+                    type="password" 
+                    id="password"
+                    placeholder="Password" 
+                    class="mt-1 p-2 w-full border rounded-md">
+            </div>
+
+            <button class="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600">Login</button>
+        </div>
     </div>
 </template>
+
 
 <style scoped>
 
