8ca06a4ed763812a959e3db101078a8c1c4c39b8
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-08 22:17:16 +0200

    Update index.vue

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,13 +1,27 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, reactive } from 'vue'
 
-const displayPage = ref(false)  // A reactive property that decides whether to display the page
+const displayPage = ref(false)
+const reports = reactive([])  // Store reports that match the 'validUser' email
 
 onMounted(() => {
   const user = localStorage.getItem('validUser')
 
-  // If the key exists and its value isn't 'undefined', display the content
-  displayPage.value = user && user !== 'undefined' // sets the value of displayPage based on the condition provided. If user is not undefined, displayPage will be true
+  // Check if 'validUser' exists in localStorage
+  displayPage.value = user && user !== 'undefined'
+
+  // Retrieve all reports that match the email
+  if (displayPage.value) {
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i)
+      if (key !== 'validUser') {
+        const storedData = JSON.parse(localStorage.getItem(key))
+        if (storedData && storedData.email === user) {
+          reports.push(storedData.missionData)
+        }
+      }
+    }
+  }
 })
 </script>
 
@@ -16,13 +30,15 @@ onMounted(() => {
     <h2 class="mt-4">Dashboard</h2>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus dolorum esse voluptatibus minima odio iste commodi cupiditate sed quidem incidunt?</p>
 
-    <div>
-        <div class="flex space-x-5">
-        <p>report placeholder 1</p>
+    <!-- Loop through the reports and display them -->
+    <div v-for="report in reports" :key="report.missionName">
+      <div class="flex space-x-5">
+        <p>{{ report.missionName }}</p>
         <button class="bg-blue-500 px-2 py-1 rounded-md">Update</button>
         <button class="bg-red-500 px-2 py-1 rounded-md">Delete</button>
-        </div>
+      </div>
     </div>
+
     <div>
         <NuxtLink to="/reports/missionDetails"><button class="bg-blue-500 px-2 py-1 rounded-md">Create Report</button></NuxtLink>
     </div>
@@ -31,5 +47,5 @@ onMounted(() => {
 </template>
 
 <style scoped>
-
+  /* Your styles here */
 </style>
