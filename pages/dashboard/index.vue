64ea84c06dfca2024291b3034b399d994a15cb3f
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-08 13:57:19 +0200

    Update index.vue

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -15,8 +15,19 @@ onMounted(() => {
   <div v-if="displayPage">
     <h2 class="mt-4">Dashboard</h2>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus dolorum esse voluptatibus minima odio iste commodi cupiditate sed quidem incidunt?</p>
+
+    <div>
+        <div class="flex space-x-5">
+        <p>report placeholder 1</p>
+        <button class="bg-blue-500 px-2 py-1 rounded-md">Update</button>
+        <button class="bg-red-500 px-2 py-1 rounded-md">Delete</button>
+        </div>
+    </div>
+    <div>
+        <button class="bg-blue-500 px-2 py-1 rounded-md">Create Report</button>
+    </div>
   </div>
-  <p v-else>Protected Route</p>
+  <p class="text-red-500" v-else>Protected Route</p>
 </template>
 
 <style scoped>
