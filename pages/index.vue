ff6f6c02ae01361fe69e3a74b15ebb4e42efc985
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-08 09:51:52 +0200

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus dolorum esse voluptatibus minima odio iste commodi cupiditate sed quidem incidunt?</p>
+    </div>
+</template>
+
+<style scoped>
+
+    h2{
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p{
+        font-size: 18px;
+        margin: 20px 0;
+    }
+</style>
