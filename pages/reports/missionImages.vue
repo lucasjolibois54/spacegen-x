2467db6ad3376756f1d88579c13886a1a5f206a1
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-09 14:53:23 +0200

    Update missionImages.vue

--- pages/reports/missionImages.vue
+++ pages/reports/missionImages.vue
@@ -1,31 +1,32 @@
 <script setup>
-import { ref } from 'vue';
-
+import { ref } from "vue";
 
 //STORE & ROUTING
-import { useMissionStore } from '~/stores/mission'
+import { useMissionStore } from "~/stores/mission";
 
-const missionStore = useMissionStore()
+const missionStore = useMissionStore();
 
-const displayPage = ref(false)
+const displayPage = ref(false);
 
 onMounted(() => {
-  const user = localStorage.getItem('validUser')
-  displayPage.value = user && user !== 'undefined'
-})
+  missionImages.value = [...missionStore.missionImages];
 
+  const user = localStorage.getItem("validUser");
+  displayPage.value = user && user !== "undefined";
+});
 
 const saveMissionData = () => {
   missionStore.updateMissionImages(missionImages.value);
-}
+};
 
-
-const { data: imageData } = useFetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=DEMO_KEY&sol=15');
+const { data: imageData } = useFetch(
+  "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=DEMO_KEY&sol=15"
+);
 const missionImages = ref([]); // ref to hold the selected images
 
 const toggleImageSelection = (img_src) => {
   if (missionImages.value.includes(img_src)) {
-    missionImages.value = missionImages.value.filter(img => img !== img_src);
+    missionImages.value = missionImages.value.filter((img) => img !== img_src);
   } else {
     missionImages.value.push(img_src);
   }
@@ -35,15 +36,34 @@ const toggleImageSelection = (img_src) => {
 <template>
   <div>
     <h2>Mission Images</h2>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus dolorum esse voluptatibus minima odio iste commodi cupiditate sed quidem incidunt?</p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus dolorum
+      esse voluptatibus minima odio iste commodi cupiditate sed quidem incidunt?
+    </p>
 
     <div class="image-grid" v-if="imageData && imageData.photos">
-      <div v-for="image in imageData.photos" :key="image.id" @click="toggleImageSelection(image.img_src)" :class="{ 'selected-image': missionImages.includes(image.img_src) }">
-        <img :src="image.img_src" alt="Mars Rover Image" class="mission-image" />
+      <div
+        v-for="image in imageData.photos"
+        :key="image.id"
+        @click="toggleImageSelection(image.img_src)"
+        :class="{ 'selected-image': missionImages.includes(image.img_src) }"
+      >
+        <img
+          :src="image.img_src"
+          alt="Mars Rover Image"
+          class="mission-image"
+        />
       </div>
     </div>
   </div>
-  <NuxtLink to="/reports/missionISSPosition"><button @click="saveMissionData" class="mt-4 bg-blue-500 px-2 py-1 rounded-md text-white">Next Step</button></NuxtLink>
+  <NuxtLink to="/reports/missionISSPosition"
+    ><button
+      @click="saveMissionData"
+      class="mt-4 bg-blue-500 px-2 py-1 rounded-md text-white"
+    >
+      Next Step
+    </button></NuxtLink
+  >
 </template>
 
 <style scoped>
