15a0bd9a4a4b6a8028991bd58b0d3e4d5a175d5c
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-09 11:57:29 +0200

    Update finalizeReport.vue

--- pages/reports/finalizeReport.vue
+++ pages/reports/finalizeReport.vue
@@ -1,12 +1,12 @@
 <script setup>
-import { ref } from 'vue';
-import { useMissionStore } from '~/stores/mission.js';
+import { ref } from "vue";
+import { useMissionStore } from "~/stores/mission.js";
 
 const missionStore = useMissionStore();
 
 const saveToLocalStorage = () => {
   // Fetch validUser value from localStorage
-  const email = localStorage.getItem('validUser');
+  const email = localStorage.getItem("validUser");
 
   // Generate an auto-incremented ID for localStorage
   let id = 1; // start with 1
@@ -23,13 +23,16 @@ const saveToLocalStorage = () => {
       missionDate: missionStore.missionDate,
       missionImages: missionStore.missionImages,
       missionLongitude: missionStore.missionLongitude,
-      missionLatitude: missionStore.missionLatitude
-    }
+      missionLatitude: missionStore.missionLatitude,
+    },
   };
 
   // Save data object to localStorage
   localStorage.setItem(id.toString(), JSON.stringify(data));
-}
+
+  // Reset the store's state
+  missionStore.resetState();
+};
 </script>
 
 <template>
@@ -43,10 +46,17 @@ const saveToLocalStorage = () => {
     <div>
       <h2>Selected Images:</h2>
       <div v-if="missionStore.missionImages.length">
-        <img v-for="(imgSrc, index) in missionStore.missionImages" :key="index" :src="imgSrc" alt="Mission Image" />
+        <img
+          v-for="(imgSrc, index) in missionStore.missionImages"
+          :key="index"
+          :src="imgSrc"
+          alt="Mission Image"
+        />
       </div>
       <p v-else>No images selected.</p>
     </div>
-    <NuxtLink to="/dashboard"><button @click="saveToLocalStorage">Save</button></NuxtLink>
+    <NuxtLink to="/dashboard"
+      ><button @click="saveToLocalStorage">Save</button></NuxtLink
+    >
   </div>
 </template>
