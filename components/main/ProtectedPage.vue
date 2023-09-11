04b5e6932ad77fdf37bbbb52c35341879b307282
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-11 10:20:24 +0200

    Create ProtectedPage.vue

---
+++ components/main/ProtectedPage.vue
@@ -0,0 +1,19 @@
+<!-- HOC-->
+<template>
+    <div v-if="displayPage">
+      <slot></slot>
+    </div>
+    <h1 class="text-red-500" v-else>This page is protected</h1>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue'
+  
+  const displayPage = ref(false)
+  
+  onMounted(() => {
+    const user = localStorage.getItem('validUser')
+    displayPage.value = user && user !== 'undefined'
+  })
+  </script>
+  
