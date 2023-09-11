8498dc3459267a95386146e2f5c078d09041dbab
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-11 09:52:23 +0200

    Create oldDashboard.vue

---
+++ old-pages/oldDashboard/oldDashboard.vue
@@ -0,0 +1,98 @@
+<script setup>
+import { ref, onMounted, reactive } from 'vue'
+
+
+import { useRouter } from 'vue-router';
+import { useMissionStore } from '~/stores/mission';
+
+const router = useRouter();
+const missionStore = useMissionStore();
+
+const updateReport = (id) => {
+  const storedReport = JSON.parse(localStorage.getItem(id));
+  
+  if(storedReport && storedReport.missionData) {
+    missionStore.populateStoreWithReport(storedReport.missionData);
+  }
+  
+  missionStore.setCurrentReportId(id);
+  router.push('/reports/missionDetails');
+};
+
+
+
+const displayPage = ref(false)
+const reports = reactive([])  // Store reports with associated ID for deletion purpose
+const currentUser = ref(null)
+
+async function fetchUserData(email) {
+    try {
+        const response = await fetch('/data/users/users.json')
+        if (!response.ok) {
+            throw new Error('Failed to fetch user data')
+        }
+        const users = await response.json()
+        return users.find(user => user.email === email)
+    } catch (error) {
+        console.error('There was a problem fetching the user data:', error)
+    }
+}
+
+function deleteReport(reportId) {
+    localStorage.removeItem(reportId)
+    const index = reports.findIndex(report => report.id === reportId)
+    if (index !== -1) {
+        reports.splice(index, 1)
+    }
+}
+
+onMounted(async () => {
+  const user = localStorage.getItem('validUser')
+  displayPage.value = user && user !== 'undefined'
+
+  if (displayPage.value) {
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i)
+      if (key !== 'validUser') {
+        const storedData = JSON.parse(localStorage.getItem(key))
+        if (storedData && storedData.email === user) {
+          reports.push({ ...storedData.missionData, id: key })
+        }
+      }
+    }
+
+    currentUser.value = await fetchUserData(user)
+  }
+})
+</script>
+
+<template>
+    <div v-if="displayPage">
+      <h2 class="mt-4">Dashboard</h2>
+  
+      <div v-if="currentUser">
+        <img :src="currentUser.avatar" alt="User Avatar" width="50">
+        <h3>{{ currentUser.first_name }} {{ currentUser.last_name }}</h3>
+      </div>
+      
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+  
+      <div v-for="report in reports" :key="report.id">
+        <div class="flex space-x-5 my-3">
+            <p>ID: #{{ report.id }}</p>
+          <p>{{ report.missionName }}</p>
+          <p>{{ report.missionDate }}</p>
+          <button @click="updateReport(report.id)" class="bg-blue-500 px-2 py-1 rounded-md">Update</button>
+          <button @click="deleteReport(report.id)" class="bg-red-500 px-2 py-1 rounded-md">Delete</button>
+        </div>
+      </div>
+  
+      <div>
+          <NuxtLink to="/reports/missionDetails"><button class="bg-blue-500 px-2 py-1 rounded-md">Create Report</button></NuxtLink>
+      </div>
+    </div>
+    <p class="text-red-500" v-else>Protected Route</p>
+</template>
+
+<style scoped>
+</style>
