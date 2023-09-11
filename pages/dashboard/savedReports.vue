2f2bbb3b99cec2434336c616c7abf2610835a693
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-11 09:52:17 +0200

    Create savedReports.vue

---
+++ pages/dashboard/savedReports.vue
@@ -0,0 +1,15 @@
+<script setup>
+definePageMeta({
+  layout: "dashboard",
+});
+</script>
+
+<template>
+    <div>
+        <h1>Saved Reports</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
