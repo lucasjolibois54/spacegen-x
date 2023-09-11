fcd18fa92ab3be9db438cab6873a38c2c3acc750
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-11 22:33:41 +0200

    Update login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -32,28 +32,28 @@ const loginUser = () => {
 </script>
 
 <template>
-    <div class="min-h-screen flex items-center justify-center bg-gray-200 p-6">
-        <div class="bg-white p-8 rounded shadow-md w-96">
-            <h1 class="text-2xl mb-4">Login</h1>
+    <div class="min-h-screen flex items-center justify-center p-6">
+        <div class=" bg-dark-btn border-2 border-dark-btn-border p-8 rounded shadow-md w-96">
+            <h1 class="text-2xl mb-4 text-main-text">Login</h1>
 
             <div class="mb-4">
-                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                <label for="email" class="block text-sm font-medium text-gray-400">Email</label>
                 <input 
                     type="email" 
                     id="email"
                     v-model="email" 
                     placeholder="you@example.com" 
-                    class="mt-1 p-2 w-full border rounded-md">
+                    class="mt-1 p-2 w-full border rounded-md bg-[#606060]/70 text-main-text">
             </div>
 
             <div class="mb-6">
-                <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                <label for="password" class="block text-sm font-medium text-gray-400">Password</label>
                 <input 
                     type="password" 
                     id="password"
                     v-model="password"
                     placeholder="Password" 
-                    class="mt-1 p-2 w-full border rounded-md">
+                    class="mt-1 p-2 w-full border rounded-md bg-[#606060]/70 text-[#CECFC2]">
             </div>
 
             <NuxtLink to="/dashboard"><button @click="loginUser" class="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600">Login</button></NuxtLink>
