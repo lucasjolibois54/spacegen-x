afa00df0ff474df63b2b834cc277f745f49422ba
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-14 09:48:12 +0200

    Update finalizeReport.vue

--- pages/reports/finalizeReport.vue
+++ pages/reports/finalizeReport.vue
@@ -19,7 +19,6 @@ const isDataComplete = computed(() => {
   );
 });
 
-
 const missionStore = useMissionStore();
 
 const saveToLocalStorage = (isDraft = false) => {
@@ -43,7 +42,7 @@ const saveToLocalStorage = (isDraft = false) => {
       missionImages: missionStore.missionImages,
       missionLongitude: missionStore.missionLongitude,
       missionLatitude: missionStore.missionLatitude,
-      savedAsDraft: isDraft  // <-- Added savedAsDraft property
+      savedAsDraft: isDraft,
     },
   };
 
@@ -57,30 +56,39 @@ const saveToLocalStorage = (isDraft = false) => {
 
 <template>
   <ProtectedPage>
-<div class="h-full bg-dark-bg flex">
-  <!-- Sidebar -->
-  <Sidebar/>
+    <div class="h-full bg-dark-bg flex">
+      <!-- Sidebar -->
+      <Sidebar />
 
-  <!-- Main Content -->
-  <div class="ml-20 md:ml-64 px-8 md:px-4 pb-8 w-full min-h-[calc(100vh-297px)]">
-      <!-- <h2 class="text-3xl font-semibold text-dashboard-title mt-12 mb-6">All My Space Reports</h2> -->
-      
-      <section>
-        <div class="mt-16">
-                <p class="text-dashboard-gray mb-0">Report Summary</p>
-                <h2 class="text-4xl font-semibold text-dashboard-title mb-0">{{ missionStore.missionName }}</h2>
-            </div>
+      <!-- Main Content -->
+      <div
+        class="ml-20 md:ml-64 px-8 md:px-4 pb-8 w-full min-h-[calc(100vh-297px)]"
+      >
+        <!-- <h2 class="text-3xl font-semibold text-dashboard-title mt-12 mb-6">All My Space Reports</h2> -->
+
+        <section>
+          <div class="mt-16">
+            <p class="text-dashboard-gray mb-0">Report Summary</p>
+            <h2 class="text-4xl font-semibold text-dashboard-title mb-0">
+              {{ missionStore.missionName }}
+            </h2>
+          </div>
         </section>
 
-      <div class="text-gray-300 mt-5">
-    <p><b>Description:</b> {{ missionStore.missionDescription }}</p>
-    <p><b>Date:</b> {{ missionStore.missionDate }}</p>
-    <p><b>Longitude:</b> {{ missionStore.missionLongitude }}</p>
-    <p><b>Latitude:</b> {{ missionStore.missionLatitude }}</p>
+        <div class="text-gray-300 mt-5">
+          <p><b>Description:</b> {{ missionStore.missionDescription }}</p>
+          <p><b>Date:</b> {{ missionStore.missionDate }}</p>
+          <p><b>Longitude:</b> {{ missionStore.missionLongitude }}</p>
+          <p><b>Latitude:</b> {{ missionStore.missionLatitude }}</p>
 
-    <div class="mt-10">
-            <h2 class="text-2xl font-semibold mb-4 text-dashboard-title">Selected Images:</h2>
-            <div class="grid grid-cols-2 md:grid-cols-4 gap-4" v-if="missionStore.missionImages.length">
+          <div class="mt-10">
+            <h2 class="text-2xl font-semibold mb-4 text-dashboard-title">
+              Selected Images:
+            </h2>
+            <div
+              class="grid grid-cols-2 md:grid-cols-4 gap-4"
+              v-if="missionStore.missionImages.length"
+            >
               <img
                 v-for="(imgSrc, index) in missionStore.missionImages"
                 :key="index"
@@ -92,32 +100,34 @@ const saveToLocalStorage = (isDraft = false) => {
             <p v-else class="italic">No images selected.</p>
           </div>
 
-<div class="flex gap-4">
-<!-- Save button -->
-<NuxtLink to="/dashboard"
-class="border-dark-btn-border px-6 items-center justify-between border-2 mt-6 bg-blue-500 text-white p-2 rounded-md inline-block">
-  <button :disabled="!isDataComplete" @click="() => saveToLocalStorage(false)">Save</button>
-</NuxtLink>
-<!-- Save as draft button -->
-<NuxtLink to="/dashboard"
-class="border-dark-btn-border items-center justify-between border-2 mt-6 bg-dark-btn text-white p-2 rounded-md inline-block">
-  <button :disabled="!isDataComplete" @click="() => saveToLocalStorage(true)">Save Draft</button>
-</NuxtLink></div>
-  </div>
-
-
-      
-  </div>
-</div>
-</ProtectedPage>
+          <div class="flex gap-4">
+            <!-- Save button -->
+            <NuxtLink
+              to="/dashboard"
+              class="border-dark-btn-border px-6 items-center justify-between border-2 mt-6 bg-blue-500 text-white p-2 rounded-md inline-block"
+            >
+              <button
+                :disabled="!isDataComplete"
+                @click="() => saveToLocalStorage(false)"
+              >
+                Save
+              </button>
+            </NuxtLink>
+            <!-- Save as draft button -->
+            <NuxtLink
+              to="/dashboard"
+              class="border-dark-btn-border items-center justify-between border-2 mt-6 bg-dark-btn text-white p-2 rounded-md inline-block"
+            >
+              <button
+                :disabled="!isDataComplete"
+                @click="() => saveToLocalStorage(true)"
+              >
+                Save Draft
+              </button>
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </ProtectedPage>
 </template>
-
-
-
-<!--
-      // Generate an auto-incremented ID for localStorage
- /* let id = 1; // start with 1
-  while (localStorage.getItem(id.toString())) {
-    id++;
-  }*/
--->
