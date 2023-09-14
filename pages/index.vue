1c8e5189a0c51b2b0c32e1e4a6d2af617765a5fe
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-14 11:59:16 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,11 @@
 
-    <template>
-    <ProtectedPage>
+    <template>  
         <Hero/>
         <ModalVideo/>
-
-    </ProtectedPage>
   </template>
   
   <script setup>
-  import ProtectedPage from '@/components/main/ProtectedPage.vue';
   import Hero from '~/components/home/Hero.vue';
-import ModalVideo from '~/components/home/ModalVideo.vue';
+  import ModalVideo from '~/components/home/ModalVideo.vue';
   </script>
   
