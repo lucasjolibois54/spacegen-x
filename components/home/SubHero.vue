23d299b5d2a56bffd980e858cf69a9fbd59d4a75
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-22 14:26:41 +0200

    Update SubHero.vue

--- components/home/SubHero.vue
+++ components/home/SubHero.vue
@@ -27,7 +27,7 @@
           class="p-4 cursor-pointer border-2 border-dark-btn-border bg-dark-btn rounded-xl text-white my-2 overflow-hidden"
         >
         <h4 class="text-main-text font-bold">
-          How out Software help astronauts!
+          How our Software help astronauts!
         </h4>
         <div class="flex items-start relative">
             <p :class="{'max-h-0': !isActive('ComponentB'), 'max-h-[100vh]': isActive('ComponentB')}" class="text-xsm mt-2 text-gray-400 mr-6 transition-all duration-500 overflow-hidden">
@@ -81,16 +81,25 @@
       <!-- Right Column to display active component -->
       <div class="text-white">
       <div v-if="activeComponent === 'ComponentA'" data-aos="fade-up" data-aos-delay="50">
+        <img class="float-animation absolute top-5 left-5 w-3/5" src="https://res.cloudinary.com/debkwdctz/image/upload/v1695382150/Group_12_1_ijjbeb.png" />
+        <img class="float-animation absolute bottom-5 -right-20 w-4/5" src="https://res.cloudinary.com/debkwdctz/image/upload/v1695382149/Group_11_ws2vbd.png" />
         <img class="max-h-80 w-full object-cover rounded-lg" src="https://media.discordapp.net/attachments/1068131427910168670/1150722874630475826/lucasjolibois54_ultra_realstic_space_astronauta_space_mission_619a267b-d1cf-4418-931c-c860b6bbfe28.png?width=1038&height=1038"/>
       </div>
       <div v-if="activeComponent === 'ComponentB'" data-aos="fade-up" data-aos-delay="50">
-        <img class="max-h-80 w-full object-cover rounded-lg" src="https://media.discordapp.net/attachments/1068131427910168670/1150722874630475826/lucasjolibois54_ultra_realstic_space_astronauta_space_mission_619a267b-d1cf-4418-931c-c860b6bbfe28.png?width=1038&height=1038"/>
+        <img class="float-animation absolute top-5 -right-20 w-4/6" src="https://res.cloudinary.com/debkwdctz/image/upload/v1695384178/Group_14_1_xzcggx.png" />
+        <img class="float-animation absolute bottom-5 left-5 w-4/5" src="https://res.cloudinary.com/debkwdctz/image/upload/v1695384177/Group_15_dziinv.png" />
+        <img class="max-h-80 w-full object-cover rounded-lg" src="https://media.discordapp.net/attachments/1068131427910168670/1154746534353702952/lucasjolibois54_super_ultra_realistic_space_station_astronaut_s_61181e58-9add-4d64-86cf-934ca8669a95.png?width=1038&height=1038"/>
       </div>
       <div v-if="activeComponent === 'ComponentC'" data-aos="fade-up" data-aos-delay="50">
-        <img class="max-h-80 w-full object-cover rounded-lg" src="https://media.discordapp.net/attachments/1068131427910168670/1150722874630475826/lucasjolibois54_ultra_realstic_space_astronauta_space_mission_619a267b-d1cf-4418-931c-c860b6bbfe28.png?width=1038&height=1038"/>
+        <img class="float-animation absolute top-5 left-5 w-3/5" src="https://res.cloudinary.com/debkwdctz/image/upload/v1695384639/Group_14_2_u2thdz.png" />
+        <img class="float-animation absolute bottom-5 -right-20 w-4/5" src="https://res.cloudinary.com/debkwdctz/image/upload/v1695384637/Group_16_cdreuh.png" />
+        <img class="max-h-80 w-full object-cover rounded-lg" src="https://media.discordapp.net/attachments/1068131427910168670/1154744141113196585/lucasjolibois54_ultra_realstic_space_female_astronaute_space_mi_8465a0b0-92cc-4632-ae7c-a1bc7fee4d8c.png?width=1038&height=1038"/>
       </div>
       <div v-if="activeComponent === 'ComponentD'" data-aos="fade-up" data-aos-delay="50">
-        <img class="max-h-80 w-full object-cover rounded-lg" src="https://media.discordapp.net/attachments/1068131427910168670/1150722874630475826/lucasjolibois54_ultra_realstic_space_astronauta_space_mission_619a267b-d1cf-4418-931c-c860b6bbfe28.png?width=1038&height=1038"/>
+        <img class="float-animation absolute top-5 left-5 w-3/5" src="https://res.cloudinary.com/debkwdctz/image/upload/v1695385246/Group_18_1_pmiksz.png" />
+        <img class="float-animation absolute bottom-16 -right-10 w-2/6" src="https://res.cloudinary.com/debkwdctz/image/upload/v1695385247/Group_19_1_kghnxd.png" />
+        <img class="float-animation absolute bottom-5 -right-1 w-2/6" src="https://res.cloudinary.com/debkwdctz/image/upload/v1695385248/Group_20_2_wwcptr.png" />
+        <img class="max-h-80 w-full object-cover rounded-lg" src="https://res.cloudinary.com/debkwdctz/image/upload/v1695383709/Group_14_fanseg.png"/>
       </div></div>
     </div>
 </div>
@@ -111,5 +120,9 @@
   .rotate-90 {
     transform: rotate(-90deg);
   }
+
+  .float-animation {
+  animation: float 3s ease-in-out infinite;
+}
   </style>
   
