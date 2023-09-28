d64e71df41910f76ab89dce46c22267cc54eb811
lucasjolibois54 <lucasjolibois@icloud.com>
2023-09-28 09:29:35 +0200

    Update OlMap.vue

--- components/mission-map/OlMap.vue
+++ components/mission-map/OlMap.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, reactive } from 'vue';
+import { ref, onMounted, onUnmounted, reactive } from 'vue';
 import 'ol/ol.css';
 import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
@@ -23,32 +23,25 @@ onMounted(() => {
   displayPage.value = user && user !== 'undefined'
 })
 
-/*const saveMissionData = () => {
-  missionStore.setMissionData(missionLongitude.value, missionLatitude.value)
-}*/
-
 const saveMissionData = () => {
   missionStore.updateMissionCoordinates(missionLongitude.value, missionLatitude.value);
 }
 
-
-
 const missionLongitude = ref('')
 const missionLatitude = ref('')
-
 
 // MAP
 const mapContainer = ref(null);
 const issPosition = reactive({ longitude: null, latitude: null });
+
+let intervalId;
 
 async function fetchIssPosition() {
   const response = await fetch('https://api.wheretheiss.at/v1/satellites/25544/');
   const data = await response.json();
   issPosition.longitude = data.longitude;
   issPosition.latitude = data.latitude;
-
-    // Assign fetched longitude and latitude to missionLongitude and missionLatitude
-    missionLongitude.value = data.longitude;
+  missionLongitude.value = data.longitude;
   missionLatitude.value = data.latitude;
   
   return [data.longitude, data.latitude];
@@ -95,31 +88,41 @@ onMounted(async () => {
       zoom: 4,
     }),
   });
+  
+  // Set the interval to update the ISS position every minute
+  intervalId = setInterval(async () => {
+    await fetchIssPosition();
+    const newCoordinates = fromLonLat([issPosition.longitude, issPosition.latitude]);
+    pinpointFeature.setGeometry(new Point(newCoordinates));
+    map.getView().setCenter(newCoordinates);
+  }, 60000); // 60000 milliseconds = 1 minute
+});
+
+// Clear the interval when component is unmounted
+onUnmounted(() => {
+  if (intervalId) clearInterval(intervalId);
 });
 </script>
 
 <template>
-    <div class="py-3 px-3 bg-dark-btn border-2 border-dark-btn-border rounded-md mt-10" v-if="displayPage"> 
-      <div ref="mapContainer" class="map-container"></div>
-      <div v-if="issPosition">
-        <div class="mt-3">
-          <label for="missionLongitude">Longitude: </label>
-          <input class="bg-transparent" id="missionLongitude" type="text" v-model="missionLongitude" readonly />
-        </div>
-        <div>
-          <label for="missionLatitude">Latitude: </label>
-          <input class="bg-transparent" id="missionLatitude" type="text" v-model="missionLatitude" readonly />
-        </div>
-      <!-- <p><strong>Latitude:</strong> {{ issPosition.latitude }}</p> -->
+  <div class="py-3 px-3 bg-dark-btn border-2 border-dark-btn-border rounded-md mt-10" v-if="displayPage"> 
+    <div ref="mapContainer" class="map-container"></div>
+    <div v-if="issPosition">
+      <div class="mt-3">
+        <label for="missionLongitude">Longitude: </label>
+        <input class="bg-transparent" id="missionLongitude" type="text" v-model="missionLongitude" readonly />
+      </div>
+      <div>
+        <label for="missionLatitude">Latitude: </label>
+        <input class="bg-transparent" id="missionLatitude" type="text" v-model="missionLatitude" readonly />
+      </div>
       <NuxtLink to="/reports/finalizeReport"
       class="border-dark-btn-border px-6 items-center justify-between border-2 mt-6 bg-blue-500 text-white p-2 rounded-md inline-block">
       <button @click="saveMissionData">Next Step</button></NuxtLink>
-      </div>
     </div>
-  </template>
+  </div>
+</template>
   
-  
-
 <style scoped>
 .map-container {
   width: 100%;
